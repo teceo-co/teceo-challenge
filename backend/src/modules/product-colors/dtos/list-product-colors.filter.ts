37816c0d28a3c5b3d6e37d7be192a3235a216598
Rodrigo Rocha <rodrigorocha@teceo.co>
2025-09-19 18:04:43 -0300

    feat: init base entities structure

---
+++ backend/src/modules/product-colors/dtos/list-product-colors.filter.ts
@@ -0,0 +1,9 @@
+import type { SelectQueryBuilder } from "typeorm";
+import type BaseFilter from "../../../../commons/filters/base.filter";
+import type ProductColor from "../product-colors.model";
+
+export default class ListProductColorsFilter
+	implements BaseFilter<ProductColor>
+{
+	createWhere(queryBuilder: SelectQueryBuilder<ProductColor>): void {}
+}
---
+++ backend/src/modules/product-colors/product-colors.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import type { Repository, SelectQueryBuilder } from "typeorm";
+import type ListProductColorsFilter from "./dtos/list-product-colors.filter";
+import ProductColor from "./product-colors.model";
+
+@Injectable()
+export default class ProductColorsService {
+	constructor(
+			@InjectRepository(ProductColor)
+		private readonly repository: Repository<ProductColor>) {}
+
+	createQueryBuilder(alias: string): SelectQueryBuilder<ProductColor> {
+		return this.repository.createQueryBuilder(alias);
+	}
+
+	async list(filter: ListProductColorsFilter) {
+		const productColors = this.createQueryBuilder("productColor").getMany();
+		return productColors;
+	}
+}

cae4eeb32136bf5ecbb9f0b99355c9262b3e74fc
Rodrigo Rocha <rodrigorocha@teceo.co>
2025-09-26 21:09:30 -0300

    feat: implementa listagem de produtos cor

--- backend/src/modules/skus/dtos/list-skus.filter.ts
+++ backend/src/modules/skus/dtos/list-skus.filter.ts
@@ -2,6 +2,6 @@ import { SelectQueryBuilder } from 'typeorm';
 import BaseFilter from '../../../../commons/filters/base.filter';
 import Sku from '../skus.model';
 
-export default class ListSkusFilter implements BaseFilter<Sku> {
+export default class ListSkusFilter extends BaseFilter<Sku> {
   createWhere(queryBuilder: SelectQueryBuilder<Sku>): void {}
 }

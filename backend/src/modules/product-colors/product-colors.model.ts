e92045e729ff1c573b8898acee86a3d0bc732c0e
Rodrigo Rocha <rodrigorocha@teceo.co>
2025-09-29 16:29:45 -0300

    fix: usa preço mínimo dos skus do produto-cor

--- backend/src/modules/product-colors/product-colors.model.ts
+++ backend/src/modules/product-colors/product-colors.model.ts
@@ -1,7 +1,8 @@
-import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import BaseModel from '../../../commons/models/base.model';
 import Color from '../colors/colors.model';
 import Product from '../products/products.model';
+import Sku from '../skus/skus.model';
 
 @Entity('product_colors')
 export default class ProductColor extends BaseModel {
@@ -12,4 +13,10 @@ export default class ProductColor extends BaseModel {
   @ManyToOne(() => Color)
   @JoinColumn({ name: 'color_id', referencedColumnName: 'id' })
   color: Color;
+
+  @OneToMany(
+    () => Sku,
+    (sku) => sku.productColor,
+  )
+  skus: Sku[];
 }
